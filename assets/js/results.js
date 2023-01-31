03c259c6df15119f2af0b76c0de9276f158be0c6
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-01-31 20:49:41 +0000

    homepage code

---
+++ assets/js/results.js
@@ -0,0 +1,8 @@
+var marvelKey = "910ff7a2993b594f4e47981e53b2dfbf";
+var OMDbKey = "558d2320";
+
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
+
+function searchAPI ()
